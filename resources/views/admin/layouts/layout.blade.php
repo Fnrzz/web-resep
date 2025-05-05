0a3ab361e7711b21ec58a81f67d7951f1f24c6eb
Fnrzz <faridnurrganteng123@gmail.com>
2025-05-05 08:25:15 +0700

    [PUSH] styling-dashboard

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -12,7 +12,7 @@
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link rel="shortcut icon" href="{{ asset('img/icons/icon-48x48.png') }}" />
 
-  <title>Admin Dashboard</title>
+  <title>ResepKu</title>
 
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
@@ -39,106 +39,5 @@
 
   <script src="{{ asset('js/app.js') }}"></script>
   
-  <!-- Chart scripts (if needed) -->
-  <script>
-    document.addEventListener("DOMContentLoaded", function() {
-      var ctx = document.getElementById("chartjs-dashboard-line").getContext("2d");
-      var gradient = ctx.createLinearGradient(0, 0, 0, 225);
-      gradient.addColorStop(0, "rgba(215, 227, 244, 1)");
-      gradient.addColorStop(1, "rgba(215, 227, 244, 0)");
-      // Line chart
-      new Chart(document.getElementById("chartjs-dashboard-line"), {
-        type: "line",
-        data: {
-          labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-          datasets: [{
-            label: "Sales ($)",
-            fill: true,
-            backgroundColor: gradient,
-            borderColor: window.theme.primary,
-            data: [
-              2115,
-              1562,
-              1584,
-              1892,
-              1587,
-              1923,
-              2566,
-              2448,
-              2805,
-              3438,
-              2917,
-              3327
-            ]
-          }]
-        },
-        options: {
-          maintainAspectRatio: false,
-          legend: {
-            display: false
-          },
-          tooltips: {
-            intersect: false
-          },
-          hover: {
-            intersect: true
-          },
-          plugins: {
-            filler: {
-              propagate: false
-            }
-          },
-          scales: {
-            xAxes: [{
-              reverse: true,
-              gridLines: {
-                color: "rgba(0,0,0,0.0)"
-              }
-            }],
-            yAxes: [{
-              ticks: {
-                stepSize: 1000
-              },
-              display: true,
-              borderDash: [3, 3],
-              gridLines: {
-                color: "rgba(0,0,0,0.0)"
-              }
-            }]
-          }
-        }
-      });
-    });
-  </script>
-  
-  <!-- Other scripts (if needed) -->
-  <script>
-    document.addEventListener("DOMContentLoaded", function() {
-      // Pie chart
-      new Chart(document.getElementById("chartjs-dashboard-pie"), {
-        type: "pie",
-        data: {
-          labels: ["Chrome", "Firefox", "IE"],
-          datasets: [{
-            data: [4306, 3801, 1689],
-            backgroundColor: [
-              window.theme.primary,
-              window.theme.warning,
-              window.theme.danger
-            ],
-            borderWidth: 5
-          }]
-        },
-        options: {
-          responsive: !window.MSInputMethodContext,
-          maintainAspectRatio: false,
-          legend: {
-            display: false
-          },
-          cutoutPercentage: 75
-        }
-      });
-    });
-  </script>
 </body>
 </html>
